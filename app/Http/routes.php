4486bae486125d44ef31cb54691c7be066df6af7
daskonhdo <39095508+daskonhdo@users.noreply.github.com>
2018-10-23 17:04:13 -0500

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,34 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//ALUMNOS
+Route::get('/altaalumno','alumnos@altaalumno');
+
+Route::POST('/guardaalumno','alumnos@guardaalumno')->name('guardaalumno');
+
+Route::get('/reportealumnos','alumnos@reportealumnos');
+
+
+
+//GRADOS
+Route::get('/altagrados','grados@altagrados');
+
+Route::POST('/guardagrados','grados@guardagrados')->name('guardagrados');
+
+Route::get('/reportegrados','grados@reportegrados');
+
+
+//GRUPOS
+Route::get('/altagrupos','grupos@altagrupos');
+
+Route::POST('/guardagrupos','grupos@guardagrupos')->name('guardagrupos');
+
+Route::get('/reportegrupos','grupos@reportegrupos');
+
+
+//CALIFICACIONES
+Route::get('/altacalificaciones','calificaciones@altacalificaciones');
+
+Route::POST('/guardacalificaciones','calificaciones@guardacalificaciones')->name('guardacalificaciones');
+
+Route::get('/reportecalificaciones','calificaciones@reportecalificaciones');
